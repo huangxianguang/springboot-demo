b930614ec7e7cb12b5833abdb0e71ff207ecafea
huangxianguang <huangxianguang1989@163.com>
2021-01-12 13:24:12 +0800

    first commit

---
+++ src/main/java/com/App.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：hxg
+ * @ClassName App
+ * @date ：Created in 2021/1/12 13:06
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+        System.out.println("Hello SpringBoot Dyzz");
+    }
+}
